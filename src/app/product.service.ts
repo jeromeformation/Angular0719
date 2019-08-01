6405bcd759d0bc7a52779ee29aad816423230d01
Jerome <ambroise.jerome.formation@gmail.com>
2019-08-01 15:01:02 +0200

    Gestion des erreurs de l'API

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Product } from './model/product';
-import {Observable, of, throwError} from 'rxjs';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {catchError, tap} from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,7 @@ export class ProductService {
   /**
    * URL de l'api (le nom du virtualhost)
    */
-  private apiURL = 'http://api.partage.local/prodfffffucts';
+  private apiURL = 'http://api.partage.local/products';
 
   constructor(private http: HttpClient) {}
 
@@ -29,18 +29,19 @@ export class ProductService {
     ;
   }
 
+  /**
+   * Gère les erreurs liées à la communication entre l'API et Angular (HttpClient)
+   * @param error - Erreur reçue
+   */
   private handleError(error: HttpErrorResponse) {
       if (error.error instanceof ErrorEvent) {
-        // A client-side or network error occurred. Handle it accordingly.
         console.error('Erreur front : ', error.error.message);
       } else {
-        // The backend returned an unsuccessful response code.
-        // The response body may contain clues as to what went wrong,
         console.error(
           `Erreur de l'API : ${error.status}, ` +
           `contenu: ${error.error}`);
       }
-      // return an observable with a user-facing error message
+      // Message dans la console
       return throwError(error);
   }
 }
